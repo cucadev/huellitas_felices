792711dec0c9e04daa89dabca38d7684474c669d
Cucacode <mayraferrazin@isft225.edu.ar>
2025-06-17 21:17:49 -0300

    Saco fecha de registro de clientes

--- Backend/models/Cliente.js
+++ Backend/models/Cliente.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const clienteSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
+  apellido: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   telefono: String,
   direccion: String,
-  fechaRegistro: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Cliente', clienteSchema);
