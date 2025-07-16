b0b9f930f2e7aa605d4a7811f0a866df6401b170
Cucacode <mayraferrazin@isft225.edu.ar>
2025-07-15 21:03:46 -0300

    log in con plantilla pug en render

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+// Mostrar formulario login
+router.get('/login', (req, res) => {
+  res.render('login');  
+});
+
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/', userController.obtenerUsuarios);
