c846d6957433417f2830ec190f27578ef9e464b3
Cucacode <mayraferrazin@isft225.edu.ar>
2025-11-04 20:32:34 -0300

    Módulo Mascotas - Eliminar alerta Pop up

--- Backend/routes/mascotaRoutes.js
+++ Backend/routes/mascotaRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const mascotaController = require("../controllers/mascotaController");
 
 router.get("/", mascotaController.listarMascotas);
-router.get("/nueva", mascotaController.formNuevaMascota);
-router.post("/nueva", mascotaController.crearMascota);
+router.get("/nuevo", mascotaController.formNuevaMascota);
+router.post("/nuevo", mascotaController.crearMascota);
 router.get("/editar/:id", mascotaController.formEditarMascota);
 router.post("/editar/:id", mascotaController.editarMascota);
 router.get("/eliminar/:id", mascotaController.eliminarMascota);
